32fdfdc75c65ad6c5c416fb106a965530805e4be
Rode1o <dumirola.dr@gmail.com>
2021-08-26 17:47:36 -0500

    do deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,34 +2,33 @@
 """ do_pack function """
 
 from fabric.api import run, put, env
-from datetime import datetime
+from fabric.contrib import files
 import os
 
 env.hosts = ["34.139.151.160", "54.89.9.92"]
 
-
 def do_deploy(archive_path):
-    """ Distributes an archive to your web servers. """
-
+    """do_deploy
+    Args:
+        archive_path
+    """
     if not os.path.exists(archive_path):
         return False
 
-    path = "/data/web_static/releases/"
-    name = archive_path.split('.')[0].split('/')[1]
-    dest = path + name
+    data_path = '/data/web_static/releases/'
+    tmp = archive_path.split('.')[0]
+    name = tmp.split('/')[1]
+    dest = data_path + name
 
     try:
         put(archive_path, '/tmp')
         run('mkdir -p {}'.format(dest))
-
         run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
         run('rm -f /tmp/{}.tgz'.format(name))
         run('mv {}/web_static/* {}/'.format(dest, dest))
         run('rm -rf {}/web_static'.format(dest))
         run('rm -rf /data/web_static/current')
         run('ln -s {} /data/web_static/current'.format(dest))
-
         return True
-
     except:
         return False
